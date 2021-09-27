ba72406fbdd305a2aaadc113dc2e74a7102a5367
mokkiko <maksim_pro@mail.ru>
2021-09-27 22:42:36 +0300

    keeping user form when submit

--- app.rb
+++ app.rb
@@ -37,21 +37,35 @@ end
 
 post '/visit' do
   @username = params[:username]
+  @phone_number = params[:phone_number]
   @user_date = params[:user_date]
   @time1 = Time.new
   @barber = params[:barber]
   @color = params[:color]
   sex = File.open './public/users.txt', 'a'
   sex.write"Новая запись в  #{@time1}\n"
-  sex.write"User #{@username}, in chop at #{@user_date} к мастеру #{@barber}  будет краситься в #{@color}\n"
+  sex.write"User #{@username}, in chop at #{@user_date} к мастеру #{@barber}  будет краситься в #{@color} ... можете связаться с ним по номеру #{@phone_number}\n"
   sex.close
-  if @username == ""
-    @error = "Введите имя"
-    return erb :visit
+
+
+   error_hash = {
+    :username => 'Введите имя',
+    :phone_number => 'Введите телефон',
+    :user_date => 'Введите дату'
+   }
+  error_hash.each do |key, value|
+    if params[key] == ''
+      @error = error_hash[key]
+      return erb :visit
+    end
+
+
   end
 
+
+
   erb "Уважаемый дргу, #{@username}, вы записались #{@user_date} к мастеру #{@barber}  будет краситься в #{@color}\n"
-#  erb :visit
+  #  erb :visit
 end
 
 get '/login/form' do
